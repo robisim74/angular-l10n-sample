49a2072255268a018254e6b08dcd56e803a67487
Roberto Simonetti <robisim74@gmail.com>
2017-07-05 21:22:26 +0200

    Update configuration

--- build.js
+++ build.js
@@ -1,33 +1,33 @@
 "use strict";
 
-require('shelljs/global');
+const shell = require('shelljs');
 const chalk = require('chalk');
 
-echo('Start building for production...');
+shell.echo('Start building for production...');
 
-rm('-Rf', 'aot/*');
-rm('-Rf', 'dist/*');
+shell.rm('-Rf', 'aot/*');
+shell.rm('-Rf', 'dist/*');
 
 /* TSLint with Codelyzer */
 // https://github.com/palantir/tslint/blob/master/src/configs/recommended.ts
 // https://github.com/mgechev/codelyzer
-echo('Start TSLint');
-exec('tslint --project ./tsconfig.json --type-check ./src/**/*.ts -e ./src/main-aot.ts');
-echo(chalk.green('TSLint completed'));
+shell.echo('Start TSLint');
+shell.exec('tslint -c tslint.json -t stylish src/**/*.ts -e src/main-aot.ts');
+shell.echo(chalk.green('TSLint completed'));
 
 /* Aot compilation */
-echo('Start AoT compilation');
-echo('ngc -p tsconfig-aot.json');
-if (exec('ngc -p tsconfig-aot.json').code !== 0) {
-    echo(chalk.red('Error: AoT compilation failed'));
-    exit(1);
+shell.echo('Start AoT compilation');
+shell.echo('ngc -p tsconfig-aot.json');
+if (shell.exec('ngc -p tsconfig-aot.json').code !== 0) {
+    shell.echo(chalk.red('Error: AoT compilation failed'));
+    shell.exit(1);
 }
-echo(chalk.green('AoT compilation completed'));
+shell.echo(chalk.green('AoT compilation completed'));
 
 /* Tree shaking & minification with webpack */
-echo('Start Tree shaking & minification');
-echo('cross-env NODE_ENV=production webpack');
-exec('cross-env NODE_ENV=production webpack');
-echo(chalk.green('Tree shaking completed'));
+shell.echo('Start Tree shaking & minification');
+shell.echo('cross-env NODE_ENV=production webpack');
+shell.exec('cross-env NODE_ENV=production webpack');
+shell.echo(chalk.green('Tree shaking completed'));
 
-echo(chalk.green('End building'));
+shell.echo(chalk.green('End building'));
